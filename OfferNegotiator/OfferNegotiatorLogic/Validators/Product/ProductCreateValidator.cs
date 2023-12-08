836cedabc5a640d354539d0649e3d81deaa3c741
Mariusz Woźniak <mariuszwozniak145@gmail.com>
2023-12-08 15:46:31 +0100

    Added validator for Product

---
+++ OfferNegotiator/OfferNegotiatorLogic/Validators/Product/ProductCreateValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using OfferNegotiatorLogic.DTOs.Product;
+
+namespace OfferNegotiatorLogic.Validators.Product;
+
+public class ProductCreateValidator : AbstractValidator<ProductCreateDTO>
+{
+    public ProductCreateValidator()
+    {
+        RuleFor(p => p.Name)
+            .MaximumLength(255)
+            .WithMessage("Name must not have more than 255 characters.");
+
+        RuleFor(p => p.Price)
+            .GreaterThan(0)
+            .WithMessage("Price must be greater than 0.");
+    }
+}
