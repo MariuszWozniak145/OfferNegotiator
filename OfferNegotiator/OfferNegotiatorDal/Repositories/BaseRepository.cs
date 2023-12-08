f8d6a0040d40012b27b219361a53f9d12c48582e
Mariusz Woźniak <mariuszwozniak145@gmail.com>
2023-12-08 13:16:02 +0100

    Added BaseRepository

---
+++ OfferNegotiator/OfferNegotiatorDal/Repositories/BaseRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OfferNegotiatorDal.DbContexts;
+using OfferNegotiatorDal.Repositories.Interfaces;
+
+namespace OfferNegotiatorDal.Repositories;
+
+public class BaseRepository<T> : IBaseRepository<T> where T : class
+{
+    protected readonly OfferNegotiatorContext _dbContext;
+
+    public BaseRepository(OfferNegotiatorContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<List<T>> GetAllAsync()
+    {
+        return await _dbContext.Set<T>().ToListAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(Guid id)
+    {
+        return await _dbContext.Set<T>().FindAsync(id);
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        var result = await _dbContext.Set<T>().AddAsync(entity);
+        var addedEntity = result.Entity;
+        await _dbContext.SaveChangesAsync();
+        return addedEntity;
+    }
+
+    public async Task<T> UpdateAsync(T entity)
+    {
+        T? updatedEntity = null;
+        await Task.Run(() =>
+        {
+            var result = _dbContext.Set<T>().Update(entity);
+            updatedEntity = result.Entity;
+        });
+        await _dbContext.SaveChangesAsync();
+        return updatedEntity;
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        await Task.Run(() =>
+        {
+            _dbContext.Set<T>().Remove(entity);
+        });
+        await _dbContext.SaveChangesAsync();
+    }
+}
---
+++ OfferNegotiator/OfferNegotiatorDal/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace OfferNegotiatorDal.Repositories.Interfaces;
+
+public interface IBaseRepository<T> where T : class
+{
+    Task<List<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(Guid id);
+    Task<T> AddAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
+
